af31a92fde2860a6a1713041a7640ea9bc9af79a
AleG94 <AleG94@users.noreply.github.com>
2020-07-12 21:04:24 +0200

    feat: add price tracker
    Periodically poll for products with outdated info and update them

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const config = require('config');
 const database = require('./lib/database');
 const Bot = require('./lib/bot');
+const priceTracker = require('./lib/price-tracker');
 
 const mongoConnectionURI = config.get('mongo.connectionURI');
 const telegramBotToken = config.get('telegram.token');
@@ -10,4 +11,5 @@ const bot = new Bot(telegramBotToken);
 
 database.connect(mongoConnectionURI).then(() => {
   bot.launch();
+  priceTracker.start();
 });
