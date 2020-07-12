75e63e9d108919c8be4f06356467b0a8c03920d6
AleG94 <AleG94@users.noreply.github.com>
2020-07-12 21:12:22 +0200

    feat: send alert message on updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const config = require('config');
 const database = require('./lib/database');
 const Bot = require('./lib/bot');
 const priceTracker = require('./lib/price-tracker');
+const alertMessage = require('./lib/templates/alert-message');
 
 const mongoConnectionURI = config.get('mongo.connectionURI');
 const telegramBotToken = config.get('telegram.token');
@@ -12,4 +13,6 @@ const bot = new Bot(telegramBotToken);
 database.connect(mongoConnectionURI).then(() => {
   bot.launch();
   priceTracker.start();
+
+  priceTracker.on('update', product => bot.sendMessage(product.user, alertMessage.create(product)));
 });
